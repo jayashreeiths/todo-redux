1644d864258da253455f2936a07d7f3ba0ba1dd4
ghanavinodhinichandrasekaran <ghanacvino@gmail.com>
2021-10-19 17:11:20 +0200

    edit,delet functionality redux

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,6 +1,6 @@
-import React,{ useState } from "react" ;
+import React,{ useState,useRef } from "react" ;
 import { connect } from "react-redux";
-import { addTodos } from "../redux/reducer";
+import { addTodos, removeTodos, updateTodos, completeTodos } from "../redux/reducer";
 const mapStateToProps = (state) => {
     return {
       todos: state,
@@ -9,11 +9,31 @@ const mapStateToProps = (state) => {
   const mapDispatchToProps = (dispatch) => {
     return {
       addTodo: (obj) => dispatch(addTodos(obj)),
+      removeTodo: (id) => dispatch(removeTodos(id)),
+      updateTodo: (obj) => dispatch(updateTodos(obj)),
+      completeTodo: (id) => dispatch(completeTodos(id)),
     };
   };
   
   const Todos = (props) => {
       const [todo, setTodo] = useState("");
+
+      const inputRef = useRef(true); //Reference to textarea
+
+      /* To change focus to textarea and edit the the textarea content */
+      const changeFocus = () => {
+        inputRef.current.disabled = false;
+        inputRef.current.focus();
+      }
+
+      const update = (id,value,e) =>{
+        //13 is key code for ENTER key  
+        if(e.which === 13){
+            props.updateTodo( {id,item:value});
+            inputRef.current.disabled = true;
+          }
+      }
+
       const handleChange = (e)=>{
        setTodo(e.target.value)
       }
@@ -26,9 +46,25 @@ const mapStateToProps = (state) => {
                 id:Math.floor(Math.random()*1000),
                 item:todo,
                 completed:false,
-                
-
             })} >Add</button>
+            <br />
+            <ul>
+              {
+                props.todos.map(i=>{
+                  return (
+                  <li key={i.id}>
+                    <textarea 
+                    ref={inputRef} 
+                    disabled={inputRef} 
+                    defaultValue={i.item}
+                    onKeyPress={(e)=>update(i.id,inputRef.current.value,e)} />
+                    <button onClick={()=>changeFocus()}>Edit</button>
+                    <button onClick={()=>props.completeTodo(i.id)}>Complete</button>
+                    <button onClick={()=>props.removeTodo(i.id)}>Delete</button>
+                  </li>)
+                })
+              }
+            </ul>
         </div>
     )
 }
